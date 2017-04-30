0d39f0ac6772f53205c4f6a6e6e1629058758ad3
Anton Paquin <antonpaquin@gmail.com>
2017-04-30 16:43:29 -0400

    we have 5 servos

--- servo.c
+++ servo.c
@@ -5,6 +5,7 @@
  *      Author: anton
  */
 
+#include "msp.h"
 #include "driverlib.h"
 
 #define servo_min 3000
@@ -21,10 +22,10 @@
     }
 
 Timer_A_PWMConfig servo_confs[]  = {
-    SERVCONF(0),
+    SERVCONF(1), // TA1
     SERVCONF(1),
     SERVCONF(2),
-    SERVCONF(3),
+    SERVCONF(2), // TA1
     SERVCONF(4)
 };
 
@@ -34,19 +35,31 @@ void servo_set(uint8_t id, uint8_t pos) {
     if (id > 4)
         return;
     servo_confs[id].dutyCycle = servo_min + (servo_step * pos);
-    MAP_Timer_A_generatePWM(TIMER_A0_BASE, servo_confs + id);
+    if (id==0 || id==3) {
+        MAP_Timer_A_generatePWM(TIMER_A1_BASE, servo_confs + id);
+    } else {
+        MAP_Timer_A_generatePWM(TIMER_A0_BASE, servo_confs + id);
+    }
 }
 
 void servo_setup() {
-    MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2, GPIO_PIN4, GPIO_PRIMARY_MODULE_FUNCTION);
-    MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2, GPIO_PIN5, GPIO_PRIMARY_MODULE_FUNCTION);
-    MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2, GPIO_PIN6, GPIO_PRIMARY_MODULE_FUNCTION);
-    MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2, GPIO_PIN7, GPIO_PRIMARY_MODULE_FUNCTION);
-    MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2, GPIO_PIN7, GPIO_PRIMARY_MODULE_FUNCTION);
+    // Godammit driverlib
+    PMAPKEYID = 0x02D52;
 
-    MAP_Timer_A_generatePWM(TIMER_A0_BASE, servo_confs + 0);
+    P2MAP -> PMAP_REGISTER4 = 20; P2MAP -> PMAP_REGISTER5 = 21; P2MAP -> PMAP_REGISTER7 = 23;
+    uint8_t p2bits = BIT4 | BIT5 | BIT7;
+    P2->DIR |= p2bits; P2->SEL1 &= ~p2bits; P2->SEL0 |= p2bits;
+
+    P7MAP->PMAP_REGISTER7 = 24; P7MAP->PMAP_REGISTER6 = 25;
+    uint8_t p7bits = BIT6 | BIT7;
+    P7->DIR |= p7bits; P7->SEL1 &= ~p7bits; P7->SEL0 |= p7bits;
+
+    PMAPKEYID = 0;
+
+
+    MAP_Timer_A_generatePWM(TIMER_A1_BASE, servo_confs + 0);
     MAP_Timer_A_generatePWM(TIMER_A0_BASE, servo_confs + 1);
     MAP_Timer_A_generatePWM(TIMER_A0_BASE, servo_confs + 2);
-    MAP_Timer_A_generatePWM(TIMER_A0_BASE, servo_confs + 3);
+    MAP_Timer_A_generatePWM(TIMER_A1_BASE, servo_confs + 3);
     MAP_Timer_A_generatePWM(TIMER_A0_BASE, servo_confs + 4);
 }
