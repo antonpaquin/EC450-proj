1b4006d06d3b6c29e132d45ff40f7e77ebcc7a44
Anton Paquin <antonpaquin@gmail.com>
2017-04-29 20:27:31 -0400

    purge it with fire, prepare for the days to come as the prophecies fortold

--- main.c
+++ main.c
@@ -5,39 +5,10 @@
 //****************************************************************************
 
 #include "msp.h"
-#include "gamecube.h"
-#include "transfer.h"
 
 void main(void) {
 	
     WDTCTL = WDTPW | WDTHOLD;           // Stop watchdog timer
 
-    // Set clockspeed to something fast enough
-    CS->KEY = 0x695A; // Clock key enables clock modification
-    uint32_t DCOMAX = 327680;
-    CS->CTL0 |= DCOMAX; // DCO to max speed
-    CS->CTL1 &= ~BIT2; CS -> CTL1 |= BIT1; CS -> CTL1 |= BIT0; // Set mclk to DCO
-
-
-    // Set up a gpio interrupt
-    P4 -> DIR &= ~BIT5; // Set p4, 5 as an input
-    P4 -> IES &= ~BIT5; // Interrupt on posedge
-    P4 -> IE |= BIT5; // Set p4 5 interrupt on
-    NVIC -> ISER[0] = 1 << ((PORT4_IRQn) & 31); // P4 interrupt in NVIC
-
-
-    // Build a gc input
-    struct gc_input_t* simpleInput = gc_init();
-
-    // Put the gc input in the buffer
-    tf_putBuffer(simpleInput);
-
-    while(1) {
-        tf_onGpioInterrupt();
-    }
 }
 
-// Attach gpio interrupt to transfer.c
-void PORT4_IRQHandler(void) {
-    tf_onGpioInterrupt();
-}
